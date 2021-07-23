934c193bf5ed393de3ff56fb8b96051a5c58cdbb
Iago Rodrigues <yagomello16@gmail.com>
2021-07-23 19:10:06 -0300

    chore: add dscatalog theme

--- src/plugins/vuetify/index.ts
+++ src/plugins/vuetify/index.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
 import pt from 'vuetify/src/locale/pt'
+import theme from './theme'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme,
   lang: {
     locales: { pt },
     current: 'pt',
---
+++ src/plugins/vuetify/theme.ts
@@ -0,0 +1,18 @@
+import colors from 'vuetify/lib/util/colors'
+
+export default {
+  themes: {
+    light: {
+      primary: {
+        base: '#407BFF',
+        lighten4: '#C5D7FF',
+      },
+      secondary: {
+        base: colors.grey.base,
+        lighten2: '#93999C',
+        lighten5: '#F2F2F2',
+      },
+      error: '#DF5753',
+    },
+  },
+}
